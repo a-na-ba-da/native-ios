fb528c9652dc910cccb028b21a1386597cec41c5
Howlbin <qkqkrl12@gmail.com>
2023-06-18 22:27:59 +0900

    first upload

---
+++ Anabada_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anabada_iOS
+//
+//  Created by 신영재 on 2023/06/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Anabada_iOSUITests/Anabada_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Anabada_iOSUITestsLaunchTests.swift
+//  Anabada_iOSUITests
+//
+//  Created by 신영재 on 2023/06/18.
+//
+
+import XCTest
+
+final class Anabada_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
